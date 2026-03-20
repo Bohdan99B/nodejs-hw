0479bef1589f6234f3d3fa934c3f89b4b8a659b5
Bohdan99B <bozheikob@gmail.com>
2026-03-20 21:14:26 +0200

    hw2

---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const notesTags = [
+  'Work',
+  'Personal',
+  'Meeting',
+  'Shopping',
+  'Ideas',
+  'Travel',
+  'Finance',
+  'Health',
+  'Important',
+  'Todo'
+];
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    content: {
+      type: String,
+      default: '',
+      trim: true
+    },
+    tag: {
+      type: String,
+      enum: notesTags,
+      default: 'Todo'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const Note = mongoose.model('Note', noteSchema);
